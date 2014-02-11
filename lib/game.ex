c8b0b03d4286ad45043681b20d96a07782ab99df
Sri Prasanna <k.sriprasanna@gmail.com>
2014-02-11 18:48:51 -0500

    making the game fetch all the adjacent same colored boxes

--- lib/game.ex
+++ lib/game.ex
@@ -22,7 +22,7 @@ defmodule Game do
 
   defp do_place_coins_randomly_on_the_grid do
     grid = do_build_grid(0, [])
-    do_place_coin_on_the_grid_at(0, 0, grid)
+    do_randomly_place_coin_on_the_grid_at(0, 0, grid)
   end
 
   defp do_build_grid(20, grid), do: grid
@@ -31,21 +31,93 @@ defmodule Game do
     do_build_grid index + 1, List.insert_at(grid, index, [])
   end
 
-  defp do_place_coin_on_the_grid_at(_x, 20, grid), do: grid
+  defp do_randomly_place_coin_on_the_grid_at(_x, 20, grid), do: grid
 
-  defp do_place_coin_on_the_grid_at(20, y, grid) do
-    do_place_coin_on_the_grid_at 0, y + 1, grid
+  defp do_randomly_place_coin_on_the_grid_at(20, y, grid) do
+    do_randomly_place_coin_on_the_grid_at 0, y + 1, grid
   end
 
-  defp do_place_coin_on_the_grid_at(x, y, grid) do
+  defp do_randomly_place_coin_on_the_grid_at(x, y, grid) do
     row = Enum.at(grid, y) |> List.insert_at x, random_coin
     grid = List.replace_at grid, y, row
-    do_place_coin_on_the_grid_at x + 1, y, grid
+    do_randomly_place_coin_on_the_grid_at x + 1, y, grid
   end
+
 
   defp random_coin do
     :random.uniform(length(@coins)) |> :lists.nth @coins
   end
 
+  @doc """
+    Select the box from the user input. Find the cluster based on the coin selected, empty the cluster and fill coins from the top.
+    Returns new grid and the score.
+  """
+  def select_box([x, y], grid) do
+    coin_selected = coin_at [x, y], grid
+    cluster = do_find_adjacent_same_colored_coins([x, y], coin_selected, grid, []) |> Enum.sort
+    do_calculate_score_and_rearrange_grid(cluster, grid)
+  end
+
+  defp coin_at([x, y], grid) do
+    Enum.at(grid, y) |> Enum.at x
+  end
+
+  defp do_find_adjacent_same_colored_coins([x, y], coin_selected, grid, cluster) do
+    cluster = do_find_cluster([x + 1, y + 1], coin_selected, grid, cluster)
+    cluster = do_find_cluster([x + 1, y + 0], coin_selected, grid, cluster)
+    cluster = do_find_cluster([x + 1, y - 1], coin_selected, grid, cluster)
+    cluster = do_find_cluster([x + 0, y + 1], coin_selected, grid, cluster)
+    cluster = do_find_cluster([x + 0, y + 0], coin_selected, grid, cluster)
+    cluster = do_find_cluster([x + 0, y - 1], coin_selected, grid, cluster)
+    cluster = do_find_cluster([x - 1, y + 1], coin_selected, grid, cluster)
+    cluster = do_find_cluster([x - 1, y + 0], coin_selected, grid, cluster)
+    do_find_cluster([x - 1, y - 1], coin_selected, grid, cluster)
+  end
+
+  defp do_find_cluster([x, y], coin_selected, grid, cluster) do
+    coin = coin_at [x, y], grid
+    index = Enum.find_index cluster, fn(i) -> i == [x, y] end
+    if coin_selected == coin and !index, do: do_find_adjacent_same_colored_coins([x, y], coin_selected, grid, [[x, y] | cluster]), else: cluster
+  end
+
+  defp do_calculate_score_and_rearrange_grid(cluster, grid) do
+    score = do_calculate_score(cluster)
+    new_grid = do_rearrange_grid(cluster, grid)
+    [new_grid, score]
+  end
+
+  defp do_calculate_score(cluster) do
+    length_of_cluster = length(cluster)
+    multiply_by = cond do
+                    length_of_cluster > 10 -> 2
+                    length_of_cluster > 6 -> 1.8
+                    length_of_cluster > 4 -> 1.5
+                    length_of_cluster > 2 -> 1.2
+                    true -> 1
+                  end
+    length_of_cluster * multiply_by
+  end
+
+  defp do_rearrange_grid([], grid), do: grid
+
+  defp do_rearrange_grid([[x, y] | cluster], grid) do
+    grid = do_place_coin_on_the_grid_at [x, y], grid, nil
+    grid = do_fill_coins_from_top [x, y], grid
+    do_rearrange_grid cluster, grid
+  end
+
+  defp do_place_coin_on_the_grid_at([x, y], grid, coin) do
+    row = Enum.at(grid, y) |> List.replace_at x, coin
+    List.replace_at grid, y, row
+  end
+
+  defp do_fill_coins_from_top([x, 0], grid) do
+    do_place_coin_on_the_grid_at([x, 0], grid, nil)
+  end
+
+  defp do_fill_coins_from_top([x, y], grid) do
+   grid = do_place_coin_on_the_grid_at([x, y], grid, coin_at([x, y - 1], grid))
+   do_fill_coins_from_top([x, y - 1], grid)
+  end
 
 end
--- test/game_test.exs
+++ test/game_test.exs
@@ -1,6 +1,52 @@
 defmodule GameTest do
   use ExUnit.Case
 
+  @mock_grid [
+                [:green, :green, :green, :green, :green, :black, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green],
+                [:green, :green, :green, :black, :black, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green],
+                [:green, :green, :black, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green],
+                [:green, :green, :green, :black, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green],
+                [:green, :green, :green, :green, :black, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green],
+                [:green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green],
+                [:green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green],
+                [:green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green],
+                [:green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green],
+                [:green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green],
+                [:green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green],
+                [:green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green],
+                [:green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green],
+                [:green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green],
+                [:green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green],
+                [:green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green],
+                [:green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green],
+                [:green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green],
+                [:green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green],
+                [:green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green],
+             ]
+
+  @final_grid [
+                [:green, :green, nil   , nil   , nil   , nil   , :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green],
+                [:green, :green, :green, nil   , nil   , :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green],
+                [:green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green],
+                [:green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green],
+                [:green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green],
+                [:green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green],
+                [:green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green],
+                [:green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green],
+                [:green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green],
+                [:green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green],
+                [:green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green],
+                [:green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green],
+                [:green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green],
+                [:green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green],
+                [:green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green],
+                [:green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green],
+                [:green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green],
+                [:green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green],
+                [:green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green],
+                [:green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green, :green],
+             ]
+
   test "start the game with randomly placed coins on a 20x20 grid" do
     grid = Game.begin
     assert grid |> length == 20
@@ -8,5 +54,11 @@ defmodule GameTest do
     coin_at_the_first_box = grid |> List.first |> List.first
     index = Enum.find_index Game.coins, fn(x) -> x == coin_at_the_first_box end
     assert index != nil
+  end
+
+  test "when the user selects a box game should look for the cluster, empty it and fill coins from the top and calculate the score" do
+    [grid, score] = Game.select_box([3, 3], @mock_grid)
+    assert grid   == @final_grid
+    assert score  == 9
   end
 end
